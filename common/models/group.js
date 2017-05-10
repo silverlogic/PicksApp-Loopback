30d91deeff38e7f2ec2844b4f2adc197e832fd2e
Michael Sevy1 <md@tsl.io>
2017-05-10 16:50:59 -0400

    PICKS-9 Public Group Creation: added a public group model for users to create and join

---
+++ common/models/group.js
@@ -0,0 +1,139 @@
+'use strict';
+
+module.exports = function(Group) {
+  // Disable endpoints not needed
+  Group.disableRemoteMethod('upsert', true);
+  Group.disableRemoteMethod('upsertWithWhere', true);
+  Group.disableRemoteMethod('updateAll', true);
+  Group.disableRemoteMethod('updateAttributes', false);
+  Group.disableRemoteMethod('updateAttribute', false);
+  Group.disableRemoteMethod('verify', false);
+  Group.disableRemoteMethod('replaceOrCreate', true);
+  Group.disableRemoteMethod('replaceById', true);
+  Group.disableRemoteMethod('createChangeStream', true);
+  Group.disableRemoteMethod('find', true);
+  Group.disableRemoteMethod('findById', true);
+  Group.disableRemoteMethod('findOne', true);
+  Group.disableRemoteMethod('deleteById', true);
+  Group.disableRemoteMethod('confirm', true);
+  Group.disableRemoteMethod('count', true);
+  Group.disableRemoteMethod('exists', true);
+
+  /**
+  * Allows an user to join a public group.
+  * @param {number} userId Model id of the user that wants to join the group.
+  * @param {Function(Error)} callback
+  */
+  Group.prototype.join = function(userId, req, callback) {
+    var groupId = req.params['id'];
+    // Get the group from the database
+    Group.findById(groupId, function(error, group) {
+      if (error) {
+        console.log('Error finding group');
+        callback(error);
+      } else {
+        // First check if the user is already participanting
+        if (group.creator == userId) {
+          // The creator is technically already in the group
+          console.log('Creator is already in group');
+          var creatorInGroupError = new Error();
+          creatorInGroupError.status = 400;
+          creatorInGroupError.message = 'Creator is already in group.';
+          creatorInGroupError.code = 'BAD_REQUEST';
+          callback(creatorInGroupError);
+        } else if (group.participants === null) {
+          // Create the first participant
+          console.log('Adding first user');
+          var participants = [userId];
+          group.participants = participants;
+          group.save(function(error, updatedGroup) {
+            if (error) {
+              console.log('Error adding first participant');
+              callback(error);
+            } else {
+              callback(null);
+            }
+          });
+        } else if (group.participants.includes(userId)) {
+          // User is already in group
+          console.log('User is alrady in group');
+          var userInGroupError = new Error();
+          userInGroupError.status = 400;
+          userInGroupError.message = 'User is already in group.';
+          userInGroupError.code = 'BAD_REQUEST';
+          callback(userInGroupError);
+        } else {
+          // Add a new participant
+          console.log('Add additional user');
+          group.participants.push(userId);
+          group.save(function(error, updatedGroup) {
+            if (error) {
+              console.log('Error adding participant');
+              callback(error);
+            } else {
+              callback(null);
+            }
+          });
+        }
+      }
+    });
+  };
+
+  /**
+  * Finds all instances of the model based on a participant.
+  * @param {number} participantId Model id of the user that is part of a group.
+  * @param {boolean} isPrivate Boolean value that determines if a group is
+                               public or private.
+  * @param {Function(Error, array)} callback
+  */
+  Group.groupsForParticipants = function(participantId, isPrivate, callback) {
+    // Build filter object
+    var filterObject = {};
+    if (isPrivate) {
+      filterObject['isprivate'] = isPrivate;
+    }
+    // Find groups based on filter object
+    Group.find({where: filterObject}, function(error, results) {
+      if (error) {
+        console.log('Error getting objects');
+        callback(error, null);
+      } else {
+        if (participantId) {
+          results = results.filter(function(group) {
+            return group.participants.includes(participantId);
+          });
+          callback(null, results);
+        } else {
+          callback(null, results);
+        }
+      }
+    });
+  };
+
+  /**
+  * Finds all instances of the model based on a creator.
+  * @param {number} creatorId Model id of the user that created a group.
+  * @param {boolean} isPrivate Boolean value that determines if a group is
+                               public or private.
+  * @param {Function(Error, array)} callback
+  */
+  Group.groupsForCreator = function(creatorId, isPrivate, callback) {
+    // Build filter object
+    var filterObject = {};
+    if (creatorId) {
+      filterObject['creator'] = creatorId;
+    }
+    if (isPrivate) {
+      filterObject['isprivate'] = isPrivate;
+    }
+    // Find groups based on filter object
+    Group.find({where: filterObject}, function(error, results) {
+      if (error) {
+        console.log('Error getting objects');
+        callback(error, null);
+      } else {
+        callback(null, results);
+      }
+    });
+  };
+};
--- server/boot/create-database-tables.js
+++ server/boot/create-database-tables.js
@@ -2,11 +2,15 @@
 
 module.exports = function(app) {
   var dataSource = app.dataSources.ComposePostgreSQL;
-  var tables = ['User', 'AccessToken', 'ACL', 'RoleMapping',
-                'Role', 'PicksUser'];
-  // var tables = Object.keys(server.models).filter(function (model) {
-  //   return server.models[model].autoAttach == "ComposePostgreSQL";
-  // });
+  var tables = [
+    'User',
+    'AccessToken',
+    'ACL',
+    'RoleMapping',
+    'Role',
+    'PicksUser',
+    'Group',
+  ];
   console.log('Performing table creation/update');
   dataSource.autoupdate(tables, function(error, result) {
     if (error) {
@@ -14,5 +18,15 @@ module.exports = function(app) {
       throw error;
     }
     console.log('Tables created/updated');
+    console.log('Perfoming model relation migrations');
+    var groupSql = 'ALTER TABLE \"Group\" ADD CONSTRAINT fk_picksUser FOREIGN ';
+    groupSql += 'KEY (creator) REFERENCES \"PicksUser\" (id);';
+    dataSource.connector.query(groupSql, function(error) {
+      if (error) {
+        console.log('Relationship already exists for creator in Group');
+      } else {
+        console.log('Relationships created');
+      }
+    });
   });
 };
