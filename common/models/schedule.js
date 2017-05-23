cca2ec1e398e408254b6cef0124af6ec4dca1658
Emanuel  Guerrero <eg@tsl.io>
2017-05-23 14:33:32 -0400

    Changed the scraper used to the new one implemented.

--- common/models/schedule.js
+++ common/models/schedule.js
@@ -3,38 +3,41 @@
 module.exports = function(Schedule) {
   /**
   * Finds historical score data based on a given season and week.
+  * @param {number} leagueType The sports league to get a schedule from. For
+                               NFL, use 0.
   * @param {number} season The season in the league represented by a year.
   * @param {number} week The week in the given season.
   * @param {Function(Error, array)} callback
   */
-  Schedule.schedule = function(season, week, callback) {
+  Schedule.historical = function(leagueType, season, week, callback) {
     var ScheduleScrapper = Schedule.app.dataSources.ScheduleScrapper;
-    ScheduleScrapper.schedule(season, week, function(error, result) {
+    ScheduleScrapper.historical(leagueType, season, week,
+                                function(error, result) {
       if (error) {
         console.log('Error retrieving schedule');
         callback(error, null);
       } else {
-        var games = result['games'];
-        callback(null, games);
+        callback(null, result);
       }
     });
   };
 
   /**
   * Finds live, granular score data on a given season and week.
+  * @param {number} leagueType The sports league to get a schedule from. For
+                               NFL, use 0.
   * @param {number} season The season in the league represented by a year.
   * @param {number} week The week in the given season.
   * @param {Function(Error, array)} callback
   */
-  Schedule.liveSchedule = function(season, week, callback) {
+  Schedule.live = function(leagueType, season, week, callback) {
     var ScheduleScrapper = Schedule.app.dataSources.ScheduleScrapper;
-    ScheduleScrapper.live(season, week, function(error, result) {
+    ScheduleScrapper.live(leagueType, season, week, function(error, result) {
       if (error) {
         console.log('Error getting live schedule');
         callback(error, null);
       } else {
-        var games = result['games'];
-        callback(null, games);
+        callback(null, result);
       }
     });
   };
