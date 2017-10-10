13fdf2924624b3d7bf40fb9cf60baa5f9b85ffdb
Sunday Nwuguru <sundayguru@gmail.com>
2017-10-10 10:46:24 +0100

    PICKS-285: add a cron job to update rush and pass yards

--- server/boot/10-nfl-weather-update.js
+++ server/boot/10-nfl-weather-update.js
@@ -5,7 +5,7 @@ var utils = require('../utils');
 
 function getTeamLocation(name) {
   for (var i = 0; i < utils.teamMap.length; i++) {
-    if (utils.teamMap[i].name === name){
+    if (utils.teamMap[i].name === name) {
       return utils.teamMap[i];
     }
   }
@@ -19,16 +19,21 @@ module.exports = function(app) {
   var year = 2001;
   var week = 2;
 
-  cron.schedule('0 0 * * * *', function () {
+  cron.schedule('0 0 * * * *', function() {
     // Get current season and week in NFL
     console.log('Fetching weather data');
     if (year < 2018 && week < 18) {
-      Schedule.find({where: {season: year, week: week}}).then(function (schedules) {
-      schedules.forEach(function (schedule) {
+      Schedule.find({
+        where: {season: year, week: week},
+      }).then(function(schedules) {
+      schedules.forEach(function(schedule) {
         var teamLocation = getTeamLocation(schedule.homeTeamName);
         request.get('http://api.openweathermap.org/data/2.5/weather')
-          .query({lat: teamLocation.location.lat, lon: teamLocation.location.lon, appid: apiKey}) // query string
-          .end(function (err, res) {
+          .query({
+            lat: teamLocation.location.lat,
+            lon: teamLocation.location.lon, appid: apiKey,
+          }) // query string
+          .end(function(err, res) {
             if (!res) {
               console.log('No response from weather api');
               return;
@@ -47,23 +52,24 @@ module.exports = function(app) {
             Weather.upsertWithWhere(
               {team: teamLocation.name, createdDate: todayDate},
               {
-                team: teamLocation.name, location: teamLocation.location, dome: teamLocation.dome,
+                team: teamLocation.name,
+                location: teamLocation.location, dome: teamLocation.dome,
                 condition: condition, lastModified: new Date(),
                 createdDate: todayDate,
               }
             )
-              .then(function (weather) {
+              .then(function(weather) {
                 schedule.weather = weather;
                 schedule.save();
                 console.log('added weather details successfully');
-                if (week < 18){
+                if (week < 18) {
                   week++;
                 } else if (week > 17) {
                   week = 1;
                   year++;
                 }
               })
-              .catch(function (error) {
+              .catch(function(error) {
                 console.log('Error creating weather', error);
               });
           });
@@ -76,4 +82,4 @@ module.exports = function(app) {
     }
     console.log('Finished boot script 10');
   });
-}
+};
